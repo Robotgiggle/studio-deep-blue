38d0221f88d6665d58c718b4224ec0a5979f034f
Git-HorizonZz <oreagn10@gmail.com>
2021-08-13 18:21:00 -0400

    slowbot anim walk

--- Assets/DavidFiles/EnemyModels/slow_tank/animations/slowbotAnim.cs
+++ Assets/DavidFiles/EnemyModels/slow_tank/animations/slowbotAnim.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class slowbotAnim : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Animator slowBot;
+    public float speed = 0;
+
     void Start()
     {
-        
+        slowBot = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        AnimCheck();
+        //speed = get speed variable from Henry's enemy AI script
+    }
+
+    void AnimCheck()
+    {
+        if (speed > 0)
+        {
+            slowBot.SetBool("isMoving", true);
+        } else
+        {
+            slowBot.SetBool("isMoving", false);
+        }
     }
 }
