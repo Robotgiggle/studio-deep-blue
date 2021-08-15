9c1596049bdf98150e682a9b31914ea3eb740a05
Herobrine31 <88736866+Herobrine31@users.noreply.github.com>
2021-08-14 21:07:24 -0400

    Add files via upload

---
+++ Assets/HenryFiles/Scripts/HealthScript.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+
+    //public enum deathAction { loadLevelWhenDead, doNothingWhenDead };
+
+    public float healthPoints = 20f;
+    public float respawnHealthPoints = 20f;      //base health points
+
+    public int numberOfLives = 1;                   //lives and variables for respawning
+    public bool isAlive = true;
+
+    public GameObject explosionPrefab;
+
+    //public deathAction onLivesGone = deathAction.doNothingWhenDead;
+
+    public string LevelToLoad = "";
+
+    private Vector3 respawnPosition;
+    private Quaternion respawnRotation;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        // store initial position as respawn location
+        respawnPosition = transform.position;
+        respawnRotation = transform.rotation;
+
+        if (LevelToLoad == "") // default to current scene 
+        {
+            //SceneManager.LoadScene(0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (healthPoints <= 0)
+        {               // if the object is 'dead'
+            numberOfLives--;                    // decrement # of lives, update lives GUI
+
+            if (explosionPrefab != null)
+            {
+                Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            }
+
+            if (numberOfLives > 0)
+            { // respawn
+                transform.position = respawnPosition;   // reset the player to respawn position
+                transform.rotation = respawnRotation;
+                healthPoints = respawnHealthPoints; // give the player full health again
+            }
+            else
+            { // here is where you do stuff once ALL lives are gone)
+                isAlive = false;
+                /**
+                switch (onLivesGone)
+                {
+                    case deathAction.loadLevelWhenDead:
+                        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                        break;
+                    case deathAction.doNothingWhenDead:
+                        // do nothing, death must be handled in another way elsewhere
+                        break;
+                }*/
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void ApplyDamage(float amount)
+    {
+        healthPoints = healthPoints - amount;
+        //GlobalHealth.CurrentHealth -= (int) amount;
+    }
+
+    public void ApplyHeal(float amount)
+    {
+        healthPoints = healthPoints + amount;
+        //GlobalHealth.CurrentHealth += (int) amount;
+    }
+
+    public void ApplyBonusLife(int amount)
+    {
+        //    numberOfLives = numberOfLives + amount;
+    }
+
+    public void updateRespawn(Vector3 newRespawnPosition, Quaternion newRespawnRotation)
+    {
+        respawnPosition = newRespawnPosition;
+        respawnRotation = newRespawnRotation;
+    }
+}
