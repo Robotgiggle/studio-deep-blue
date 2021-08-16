23e1153ca61e9aa4f4d6852333375a3b3295c573
Herobrine31 <88736866+Herobrine31@users.noreply.github.com>
2021-08-16 13:47:26 -0400

    Add files via upload

--- Assets/HenryFiles/Scripts/Enemy/FastRobotScript.cs
+++ Assets/HenryFiles/Scripts/Enemy/FastRobotScript.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class FastRobotScript : MonoBehaviour
 {
-    private Animation m_Animation;
-    private Animation m_animation;
+    public Animation m_WAnimation;
+    public Animation m_Walk;
+    public Animation m_IAnimation;
+    public Animation m_Idle;
+    public Animation m_MAnimation;
+    public Animation m_Melee;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +23,29 @@ public class FastRobotScript : MonoBehaviour
 
     }
 
-    private void PlayAnimation()
+    private void PlayMAnimation()
     {
-        if (m_animation)
+        if (m_Melee)
         {
-            m_Animation = GetComponent<Animation>();
-            m_Animation.Play();
+            m_MAnimation = GetComponent<Animation>();
+            m_MAnimation.Play();
+        }
+    }
+
+    private void PlayWAnimation()
+    {
+        if (m_Walk)
+        {
+            m_WAnimation = GetComponent<Animation>();
+            m_WAnimation.Play();
+        }
+    }
+    private void PlayIAnimation()
+    {
+        if (m_Idle)
+        {
+            m_IAnimation = GetComponent<Animation>();
+            m_IAnimation.Play();
         }
     }
 }
