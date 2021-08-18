9e84f237f570aebc8e65af2f236ca4553e18ecac
Robotgiggle <purplemrb12@gmail.com>
2021-08-18 00:04:20 -0400

    Created minion spawner for behemoth
    Created a minionSpawner prefab, complete with controller script. It should be childed to the Behemoth, just like the hitbox and target, in order to work properly.

---
+++ Assets/BenFiles/Scripts/MinionSpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionSpawner : MonoBehaviour
+{
+    public GameObject minion;
+    public float spawnCooldown;
+    RaycastHit spawnPoint;
+    float tBuffer;
+    float angle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tBuffer = spawnCooldown;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time>=tBuffer){
+            angle = Random.Range(0f,359f);
+            transform.Rotate(0,angle,0,Space.World);
+            if(Physics.Raycast(transform.position,transform.forward,out spawnPoint,30)){
+                Instantiate(minion,spawnPoint.point,transform.parent.rotation);
+            }
+            tBuffer = Time.time + spawnCooldown;
+        }
+    }
+}
