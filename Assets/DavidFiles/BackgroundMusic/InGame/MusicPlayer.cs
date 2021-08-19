b2a485cee94554b0ab120cf6c162179fc56686e5
Git-HorizonZz <oreagn10@gmail.com>
2021-08-19 17:04:23 -0400

    Music in Battle

---
+++ Assets/DavidFiles/BackgroundMusic/InGame/MusicPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioClip[] clips;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
