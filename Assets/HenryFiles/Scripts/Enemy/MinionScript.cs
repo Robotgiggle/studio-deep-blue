b8cf137bcd8e8e47674a5196a11999974e70be07
Robotgiggle <purplemrb12@gmail.com>
2021-08-18 23:26:42 -0400

    Minion can now attack the core

--- Assets/HenryFiles/Scripts/Enemy/MinionScript.cs
+++ Assets/HenryFiles/Scripts/Enemy/MinionScript.cs
@@ -5,30 +5,42 @@ using UnityEngine;
 public class MinionScript : MonoBehaviour
 {
     public Transform Player;
+    public Transform core;
     public float speed = 4f;
     float actualSpeed;
     float nextAttack;
     public bool canAttack = true; //true;
     public bool isAttacking;
-    public Transform target;
+    Transform target;
     public float enemyAttackRange = 2.0f;
+    public float sightRange;
     public GameObject meleeObject;
     public bool dead = false;
-    float tBuffer;
     Vector3 muzzle;
     int mask = 1 << 6;
     public float attackCooldown;
     public float attackDamage;
+    float playerRange;
+    float coreRange;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerRange = enemyAttackRange;
+        coreRange = enemyAttackRange * 0.7f;
         if (Player == null)
         {
-
             if (GameObject.FindWithTag("Player") != null)
             {
                 Player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+            }
+        }
+
+        if (core == null)
+        {
+            if (GameObject.FindWithTag("coreTargetTag") != null)
+            {
+                core = GameObject.FindWithTag("coreTargetTag").GetComponent<Transform>();
             }
         }
     }
@@ -64,11 +76,18 @@ public class MinionScript : MonoBehaviour
                 Object.Destroy(gameObject);
             }
         }
-
-        Vector3 displacement = Player.position - transform.position;
-        displacement = displacement.normalized;
-
-        if ((Vector3.Distance(Player.position, transform.position) > enemyAttackRange))
+        //if near the core, shrink sightRange
+        if(Vector3.Distance(core.position,transform.position)<5.5f){sightRange = 6;}
+        //select core or player as target
+        if(Vector3.Distance(Player.position,transform.position)<sightRange){
+            target = Player;
+            enemyAttackRange = playerRange;
+        }else{
+            target = core;
+            enemyAttackRange = coreRange;
+        }
+        //select whether to attack or move
+        if ((Vector3.Distance(target.position, transform.position) > enemyAttackRange))
         {
             isAttacking = false;
         }
@@ -76,24 +95,24 @@ public class MinionScript : MonoBehaviour
         {
             isAttacking = true;
         }
-
-        if ((Vector3.Distance(Player.position, this.transform.position) < 1f) && dead == false)// && (Vector3.Distance(Player.position, this.transform.position) > 200.0f))
+        //perform motion
+        if ((Vector3.Distance(target.position, this.transform.position) < 1f) && dead == false)// && (Vector3.Distance(target.position, this.transform.position) > 200.0f))
         {
             actualSpeed = speed * 0.75f;
             transform.position -= transform.forward * actualSpeed * Time.deltaTime;
-            transform.LookAt(new Vector3(Player.position.x, transform.position.y, Player.position.z));
+            transform.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z));
             //transform.LookAt(Vector3(otherObject.position.x, transform.position.y, otherObject.position.z));
         }
-        else if ((Vector3.Distance(Player.position, this.transform.position) < 100.0f) && dead == false && (Vector3.Distance(Player.position, this.transform.position) > enemyAttackRange))
+        else if ((Vector3.Distance(target.position, this.transform.position) < 100.0f) && dead == false && (Vector3.Distance(target.position, this.transform.position) > enemyAttackRange))
         {
             actualSpeed = speed;
             transform.position += transform.forward * actualSpeed * Time.deltaTime;
-            transform.LookAt(new Vector3(Player.position.x, transform.position.y, Player.position.z));
+            transform.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z));
 
         }
         else if (isAttacking)
         {
-            transform.LookAt(new Vector3(Player.position.x, transform.position.y, Player.position.z));
+            transform.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z));
             actualSpeed = 0f;
         }
 
@@ -117,7 +136,7 @@ public class MinionScript : MonoBehaviour
 
     IEnumerator meleeAttack()
     {
-        if (Time.time > nextAttack && canAttack == true && (Vector3.Distance(Player.position, transform.position) < enemyAttackRange))
+        if (Time.time > nextAttack && canAttack == true && (Vector3.Distance(target.position, transform.position) < enemyAttackRange))
         {
             nextAttack = Time.time + attackCooldown;
             yield return new WaitForSeconds(attackCooldown-0.6f);
@@ -128,10 +147,12 @@ public class MinionScript : MonoBehaviour
                 if (hit.transform.gameObject.name == "core")
                 {
                     hit.transform.gameObject.GetComponent<CoreController>().takeDamage(Mathf.FloorToInt(attackDamage));
+                    /*
                     if (gameObject.name == "Minion(Clone)")
                     {
                         Destroy(gameObject, 0.3f);
                     }
+                    */
                 }
                 else if (hit.transform.gameObject.tag == "Player")
                 {
