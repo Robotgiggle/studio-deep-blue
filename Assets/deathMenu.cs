8be6a1a0eecee5ae14b1b475c68ec001b2bf8130
Herobrine31 <henry.bushong@mtsdstudent.us>
2021-08-20 06:24:40 -0400

    DeathHUDUpdate

---
+++ Assets/deathMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathMenu : MonoBehaviour
+{
+    public static bool deathHUDIsNotActive = false;
+    public GameObject deathHUD;
+    public GameObject HUD;
+    public GameObject crosshairs;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.GetComponent<HealthScript>().healthPoints <= 0 && deathHUDIsNotActive)
+        {
+            Time.timeScale = 0;
+            deathHUDIsNotActive = true;
+            crosshairs.SetActive(false);
+            HUD.SetActive(false);
+            deathHUD.SetActive(true);
+        }
+    }
+}
