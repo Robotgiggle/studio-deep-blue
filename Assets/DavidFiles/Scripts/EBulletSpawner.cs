ca7a7195c15150348fb8742611d3d96286080cd6
Robotgiggle <purplemrb12@gmail.com>
2021-08-20 16:44:29 -0400

    RangeBot nerf
    Added inaccuracy calculation so the rangebots won't always hit their targets. Also, slightly reduced rangebots' weapon range.

--- Assets/DavidFiles/Scripts/EBulletSpawner.cs
+++ Assets/DavidFiles/Scripts/EBulletSpawner.cs
@@ -6,8 +6,9 @@ public class EBulletSpawner : MonoBehaviour
 {
     public GameObject bullet;
     public float shotCooldown = 1f;
+    public float inaccuracy;
     public bool isAttacking = false;
-
+    Vector3 direction;
     float currentTime;
     float nextShotTime;
 
@@ -25,8 +26,12 @@ public class EBulletSpawner : MonoBehaviour
         //isAttacking = get shooting bool from Henry's minion enemy AI script
         if (Time.time > nextShotTime && GetComponentInParent<Enemy_1_Health>().isDead != true && GetComponentInParent<RangeRobotScript>().enemyIsInRange)
         {
+            direction = transform.rotation.eulerAngles;
+            direction.x += Random.Range(-inaccuracy,inaccuracy);
+            direction.y += Random.Range(-inaccuracy,inaccuracy);
+            direction.z += Random.Range(-inaccuracy,inaccuracy);
+            Instantiate(bullet, this.transform.position, Quaternion.Euler(direction));
             nextShotTime = Time.time + shotCooldown;
-            Instantiate(bullet, this.transform.position, this.transform.rotation);
         }
 
     }
